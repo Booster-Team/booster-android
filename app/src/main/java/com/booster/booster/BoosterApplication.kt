b4b21b06f7494e63b5ca08f0dbb60b9b6ea18bfe
kwonsongmi <ksongmi0727@gmail.com>
2024-06-26 13:40:00 +0900

    feat: Dark 테마 설정 추가

--- app/src/main/java/com/booster/booster/BoosterApplication.kt
+++ app/src/main/java/com/booster/booster/BoosterApplication.kt
@@ -1,8 +1,15 @@
 package com.booster.booster
 
 import android.app.Application
+import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BoosterApplication: Application() {
+
+    val isDark = mutableStateOf(false)
+
+    fun toggleTheme() {
+        isDark.value = !isDark.value
+    }
 }
